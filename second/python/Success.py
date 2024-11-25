28c9caffb2c35ddd89e71958f0e1edf85c3678dc
GhostDaGhost <53021768+GhostDaGhost@users.noreply.github.com>
2024-11-24 18:35:53 -0800

    modify(second): Add Python files

---
+++ second/python/Success.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def SuccessfulLogin():
+    driver = webdriver.Chrome()
+    driver.get("https://letsusedata.com")
+    time.sleep(2)
+
+    driver.find_element(By.ID, "txtUser").click()
+    driver.find_element(By.ID, "txtUser").clear()
+    driver.find_element(By.ID, "txtUser").send_keys("test1")
+    driver.find_element(By.ID, "txtPassword").click()
+    driver.find_element(By.ID, "txtPassword").clear()
+    driver.find_element(By.ID, "txtPassword").send_keys("Test12456")
+    driver.find_element(By.ID, "javascriptLogin").click()
+
+    # GIVE THE PAGE SOME TIME TO FULLY LOAD
+    time.sleep(5)
+
+# ON INITIALIZATION
+SuccessfulLogin()
